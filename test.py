d7146fea6b6942636cd356fabc532da6eaac1915
Ryan Smith <ryan.smith@ucdconnect.ie>
2021-09-21 15:58:56 +0100

    seperate training and testing files

---
+++ test.py
@@ -0,0 +1,10 @@
+"""
+Script to test a supplied PyTorch model for the DynamicTextureWaves project.
+"""
+###############################################################################
+
+# Written by Ryan Smith
+# ryan.smith@ucdconnect.ie
+# github.com/ryan597/DynamicTextureWaves
+
+###############################################################################
