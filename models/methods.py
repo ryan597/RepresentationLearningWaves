c21b3a4e49b6cf48556c19e407377c2e61664e9f
Ryan Smith <ryan.smith@ucdconnect.ie>
2021-10-06 16:12:21 +0100

    include training function, DEVICE global def

--- models/methods.py
+++ models/methods.py
@@ -1,5 +1,12 @@
+import datetime
+import torch
 import torch.nn as nn
 import torch.optim as optim
+
+if torch.cuda.is_available():
+    DEVICE = 'cuda'
+else:
+    DEVICE = 'cpu'
 
 
 class PyTorchModel():
@@ -34,7 +41,7 @@ class PyTorchModel():
 
         save_model:
 
-        evaluate_model:
+        validate_model:
 
         predict:
 
@@ -51,27 +58,91 @@ class PyTorchModel():
                  optimizer=optim.Adam,
                  scheduler=None):
         self.logs = {"loss": [], "val_loss": [], "epoch": [], "lr": []}
-        self.model = model
+        self.model = model.to(DEVICE)
         self.epochs = epochs
         self.learning_rate = learning_rate
-        self.criterion = criterion
-        self.optimizer = optimizer
-        self.scheduler = scheduler
+        self.criterion = criterion.to(DEVICE)
+        self.optimizer = optimizer(self.model.parameters(),
+                                   lr=self.learning_rate)
+        if scheduler is not None:
+            self.scheduler = scheduler(self.optimizer, scheduler_args)
 
-    def train_model(self):
-        pass
+    def train_model(self, train, valid):
+        history = {"loss": [], "val_loss": [], "epoch": [], "lr": []}
 
-    def save_model(self):
-        pass
+        criterion = nn.L1Loss().to(DEVICE)
+        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,
+                                                        T_max=epochs*3,
+                                                        eta_min=1e-7)
 
-    def evaluate_model(self):
-        pass
+        for i in range(self.epochs):
+            # update the logs
+            history["epoch"].append(i+1)
+            history["lr"].append(scheduler.get_lr())
+            # reset losses and gradients on each epoch start
+            accum_loss = 0
+            total_loss = 0
+            optimizer.zero_grad()
+            print(f"learning rate {scheduler.get_lr()}")
+
+            for j, (inputs, nxt) in enumerate(train):
+                inputs = inputs.to(DEVICE)
+                nxt = nxt.to(DEVICE)
+
+                outputs = model(inputs)
+                loss = criterion(outputs, nxt)
+                accum_loss += loss.item()
+                loss.backward()
+
+                optimizer.step()
+                optimizer.zero_grad()
+                if j % 10 == 0 and verbose:
+                    print(f"loss \t {accum_loss / 10}")
+                    total_loss += accum_loss
+                    accum_loss = 0
+
+            optimizer.step()
+            optimizer.zero_grad()
+            total_loss *= 1 / len(train)
+            history["loss"].append(total_loss)
+
+            print(f"Epoch \t {i+1} finished")
+            scheduler.step()
+            # Validation
+            self.validate_model()
+
+        return self.logs
+
+    def save_model(self, epoch, valloss):
+        timestamp = datetime.datetime.today().replace(second=0, microsecond=0)
+        torch.save(self.model.state_dict(),
+                   f"models/{epoch}_{valloss}_{timestamp}.pth")
+
+    def validate_model(self, valid):
+        with torch.no_grad():
+            validation_loss = 0
+            for i, (inputs, nxt) in enumerate(valid):
+
+                inputs = inputs.to(DEVICE)
+                nxt = nxt.to(DEVICE)
+
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, nxt)
+                validation_loss += loss.item()
+
+            validation_loss *= 1/len(valid)
+            history["val_loss"].append(valid)
+            print(f"validation loss : \t{valid_loss} \n")
 
     def predict(self, x):
         pass
 
-    def update_logs(self):
-        pass
+    def update_logs(self, key, value):
+        self.logs[key].append(value)
 
-    def get_logs(self):
-        pass
+    def get_logs(self, key=None):
+        if key is None:
+            return self.logs
+        else:
+            return self.logs[key]
