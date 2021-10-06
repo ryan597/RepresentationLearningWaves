0af25b14e0c56db515c9f94ded7794e8dcdfafe1
Ryan Smith <ryan.smith@ucdconnect.ie>
2021-10-06 17:21:06 +0100

    fix lintint errors, update docstrings

--- models/methods.py
+++ models/methods.py
@@ -14,40 +14,43 @@ class PyTorchModel():
     Class for PyTorch models to easily train, validate and test the model.
 
     Arguments:
-        model (nn.Module): An instance of nn.Module which has a defined
-            forward() method
+    model (nn.Module): An instance of nn.Module which has a defined
+        forward() method
 
-        epochs (int): The number of epochs to train for. Default value 10.
+    epochs (int): The number of epochs to train for. Default value 10.
 
-        learning_rate (double): The multiplication parameter to be passed when
-            updating weights in backpropagation. Controls how large the weights
-            updates can be. Default value 0.01
+    learning_rate (double): The multiplication parameter to be passed when
+        updating weights in backpropagation. Controls how large the weights
+        updates can be. Default value 0.01
 
-        criterion (torch.autograd.Function): Should be an instance of one of
-            the pytorch loss functions or a callable function which returns a
-            loss value. A custom function should use torch operations
-            (in loss calculation and in the forward pass) for autograd.
-            Otherwise a custom class which inherits from
-            torch.autograd.Function and defines both forward and backward
-            methods. Default value torch.nn.L1Loss()
+    criterion (torch.autograd.Function): Should be an instance of one of
+        the pytorch loss functions or a callable function which returns a
+        loss value. A custom function should use torch operations
+        (in loss calculation and in the forward pass) for autograd.
+        Otherwise a custom class which inherits from
+        torch.autograd.Function and defines both forward and backward
+        methods. Default value torch.nn.L1Loss()
 
-        optimizer (torch.optim.optimizer.Optimizer):
+    optimizer (torch.optim.optimizer.Optimizer): Function which
+        backpropagates the gradients of the loss with respect to each
+        parameter.
 
-        scheduler (): Function to call when changing the learning rate.
-            Default value None
+    scheduler (torch.optim.lr_scheduler): Function to call when changing
+        the learning rate. Default value None
 
     Methods:
-        train_model:
+        train_model (self, torch.utils.data.DataLoader: train,
+                    torch.utils.data.DataLoader: valid):
 
-        save_model:
+        save_model (self, int: epoch, double: val_loss):
 
-        validate_model:
+        validate_model (self, torch.utils.data.DataLoader: dataloader):
 
-        predict:
+        predict (self, torch.utils.data.DataLoader: dataloader):
 
-        update_logs:
+        update_logs (self, str: key, double: value):
 
-        get_logs:
+        get_logs (self, str: key=None):
 
     """
     def __init__(self,
@@ -56,7 +59,8 @@ class PyTorchModel():
                  learning_rate=0.01,
                  criterion=nn.L1Loss,
                  optimizer=optim.Adam,
-                 scheduler=None):
+                 scheduler=None,
+                 **kwargs):
         self.logs = {"loss": [], "val_loss": [], "epoch": [], "lr": []}
         self.model = model.to(DEVICE)
         self.epochs = epochs
@@ -65,64 +69,57 @@ class PyTorchModel():
         self.optimizer = optimizer(self.model.parameters(),
                                    lr=self.learning_rate)
         if scheduler is not None:
-            self.scheduler = scheduler(self.optimizer, scheduler_args)
+            self.scheduler = scheduler(self.optimizer, **kwargs)
 
-    def train_model(self, train, valid):
-        history = {"loss": [], "val_loss": [], "epoch": [], "lr": []}
-
-        criterion = nn.L1Loss().to(DEVICE)
-        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,
-                                                        T_max=epochs*3,
-                                                        eta_min=1e-7)
-
+    def train_model(self, train, valid=None):
         for i in range(self.epochs):
-            # update the logs
-            history["epoch"].append(i+1)
-            history["lr"].append(scheduler.get_lr())
+            self.update_logs("epoch", i)
+            if self.scheduler is not None:
+                self.update_logs("lr", self.scheduler.get_lr())
+            else:
+                self.update_logs("lr", self.learning_rate)
             # reset losses and gradients on each epoch start
             accum_loss = 0
             total_loss = 0
-            optimizer.zero_grad()
-            print(f"learning rate {scheduler.get_lr()}")
+            self.optimizer.zero_grad()
 
             for j, (inputs, nxt) in enumerate(train):
                 inputs = inputs.to(DEVICE)
                 nxt = nxt.to(DEVICE)
 
-                outputs = model(inputs)
-                loss = criterion(outputs, nxt)
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, nxt)
                 accum_loss += loss.item()
                 loss.backward()
 
-                optimizer.step()
-                optimizer.zero_grad()
-                if j % 10 == 0 and verbose:
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+                if j % 10 == 0:  # every 10 batches
                     print(f"loss \t {accum_loss / 10}")
                     total_loss += accum_loss
                     accum_loss = 0
 
-            optimizer.step()
-            optimizer.zero_grad()
             total_loss *= 1 / len(train)
-            history["loss"].append(total_loss)
+            self.update_logs("loss", total_loss)
 
-            print(f"Epoch \t {i+1} finished")
-            scheduler.step()
+            print(f"Epoch \t {i} finished")
+            if self.scheduler is not None:
+                self.scheduler.step()
             # Validation
-            self.validate_model()
+            if valid is not None:
+                self.validate_model()
 
         return self.logs
 
-    def save_model(self, epoch, valloss):
+    def save_model(self, epoch, val_loss):
         timestamp = datetime.datetime.today().replace(second=0, microsecond=0)
         torch.save(self.model.state_dict(),
-                   f"models/{epoch}_{valloss}_{timestamp}.pth")
+                   f"models/{epoch}_{val_loss}_{timestamp}.pth")
 
-    def validate_model(self, valid):
+    def validate_model(self, dataloader):
         with torch.no_grad():
             validation_loss = 0
-            for i, (inputs, nxt) in enumerate(valid):
+            for i, (inputs, nxt) in enumerate(dataloader):
 
                 inputs = inputs.to(DEVICE)
                 nxt = nxt.to(DEVICE)
@@ -131,18 +128,54 @@ class PyTorchModel():
                 loss = self.criterion(outputs, nxt)
                 validation_loss += loss.item()
 
-            validation_loss *= 1/len(valid)
-            history["val_loss"].append(valid)
-            print(f"validation loss : \t{valid_loss} \n")
+            validation_loss *= 1/len(dataloader)
+            self.update_logs("val_loss", validation_loss)
 
-    def predict(self, x):
-        pass
+    def predict(self, dataloader):
+        with torch.no_grad():
+            for i, inputs in enumerate(dataloader):
+                inputs = inputs.to(DEVICE)
+                return self.model(inputs)
 
     def update_logs(self, key, value):
         self.logs[key].append(value)
+        print(f"{key} : \t{value} \n")
 
     def get_logs(self, key=None):
         if key is None:
             return self.logs
         else:
             return self.logs[key]
+
+
+"""
+def show_validation_predictions(valid, model, savefile=None):
+    with torch.no_grad():
+        for i, (batch_pair, batch_nxt) in enumerate(valid):
+            fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(1, 5,
+                                                          figsize=(15, 15))
+            for j, img_pair in enumerate(batch_pair):
+                ax1.imshow(img_pair[0])
+                ax1.axis('off')
+                ax2.imshow(img_pair[1])
+                ax2.axis('off')
+                ax3.imshow(batch_nxt[0][0])
+                ax3.axis('off')
+                pr = model(batch_pair.to(DEVICE))[0][0].detach()\
+                    .to('cpu').numpy()
+                pm = (pr - np.min(pr))/(np.max(pr)-np.min(pr))
+                ax4.imshow(pm)
+                ax4.axis('off')
+                bnxt = ((batch_nxt[0][0].numpy() -
+                        np.min(batch_nxt[0][0].numpy())) /
+                        (np.max(batch_nxt[0][0].numpy()) -
+                        np.min(batch_nxt[0][0].numpy())))
+                ax5.imshow(1 - np.abs(img_pair[1] - bnxt), cmap='gray')
+                ax5.axis('off')
+                if savefile is not None:
+                    plt.savefig(f"{savefile}_{i}")
+                fig.show()
+                break
+            if i == 4:
+                break
+"""
