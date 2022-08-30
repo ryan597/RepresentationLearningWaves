7c3449e32274d3cf9720f2776732d965c56a19da
Ryan Smith <ryan.smith@ucdconnect.ie>
2022-08-30 13:40:41 +0100

    resunet

--- backbones.py
+++ backbones.py
@@ -23,7 +23,7 @@ class BasicBlock(nn.Module):
         return self.block(x) + x
 
 
-class DownsampleBlock(nn.Module):
+class ResidualLayer(nn.Module):
     def __init__(self, in_channels, out_channels, n=1, *args, **kwargs):
         super().__init__()
         self.downsample = nn.Sequential(
@@ -119,26 +119,124 @@ class ResNet_backbone(nn.Module):
 
 
 class ResUNet(nn.Module):
-    def __init__(self, in_ch, out_ch,
-                 block_size=[32, 64, 128, 256, 512, 1024],
+    """
+    Class to build the ResUNet architecture.
+    The blocks keep the channels constant while the 1x1 convolution will
+    increase the channels after the layers.
+
+    Arguments:
+        in_channels (int): The number of input channels, default value 2
+        block_sizes (list<int>): The channels in the following blocks.
+            Default value [64, 128, 256, 512, 1024]
+        depths (list<int>): The number of basic blocks to use at each layer.
+            Default value [2, 3, 5, 2]
+
+    Methods:
+        forward(self, x): x (torch.Tensor) is the sample to compute the forward
+            propagation throught the network. The tensor contains the batch
+            samples first, then channels and height and width dimensions of the
+            image.
+
+    Returns:
+        ResUNet object derived from nn.Module, can be trained in a standard
+        PyTorch training loop.
+    """
+    def __init__(self, in_channels=2, out_channels=1, masks=True, freeze=False,
+                 block_sizes=[32, 64, 128, 256, 512, 1024],
                  depths=[2, 3, 5, 3, 2]):
         super().__init__()
-        in_out_size = list(zip(block_size, block_size[1:]))
+        self.masks = masks
+        self.freeze = freeze
+        out_channels = out_channels
+        in_out_sizes = list(zip(block_sizes, block_sizes[1:]))
+        # Use a gate layer with kernel=7, wider receptive vision at start
         self.gate = nn.Sequential(
-            nn.Conv2d(in_ch, block_size[0], kernel_size=7, padding=3),
-            nn.BatchNorm2d(block_size[0]),
+            nn.Conv2d(in_channels, block_sizes[0], kernel_size=7, padding=3),
+            nn.BatchNorm2d(block_sizes[0]),
             nn.Mish(),
-            nn.Conv2d(block_size[0], block_size[0], kernel_size=3, padding=1),
-            nn.BatchNorm2d(block_size[0]),
+            nn.Conv2d(block_sizes[0], block_sizes[0], kernel_size=3,
+                      padding=1),
+            nn.BatchNorm2d(block_sizes[0]),
             nn.Mish()
         )
-
-        self.encode = nn.ModuleList([])
-        self.decode = nn.ModuleList([
-            UpsampleBlock(in_channels, in_channels, n)
-            for (in_channels, _) in in_out_size
+        encoder_layers = nn.ModuleList([
+            ResidualLayer(out_channels, out_channels, n)
+            for (in_channels, out_channels), n in zip(in_out_sizes,
+                                                      depths)
         ])
+        decoder_layers = nn.ModuleList([
+            ResidualLayer(in_channels, in_channels, n)
+            for (in_channels, out_channels), n in zip(in_out_sizes[::-1],
+                                                      [2 for _ in depths])
+        ])  # decoder layers are fixed at depth 2
+
+        self.encode = nn.ModuleList([])  # All modules must be initialised here
+        for layer, (in_chan, out_chan) in zip(encoder_layers, in_out_sizes):
+            combinedlayer = nn.Sequential(
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                nn.Conv2d(in_chan, out_chan, kernel_size=1),
+                layer
+                )
+            self.encode.append(combinedlayer)
+
+        self.decode_upsample = nn.ModuleList([])
+        self.decode = nn.ModuleList([])
+        for layer, (out_chan, in_chan) in zip(decoder_layers,
+                                              in_out_sizes[::-1]):
+            self.decode_upsample.append(nn.ConvTranspose2d(in_chan, out_chan,
+                                        kernel_size=2, stride=2))
+            combinedlayer = nn.Sequential(
+                nn.Conv2d(in_chan, out_chan, kernel_size=1),
+                layer
+            )
+            self.decode.append(combinedlayer)
+
+        self.decode.append(nn.Conv2d(in_out_sizes[0][0], 1, kernel_size=1))
+
+        if self.masks:
+            for child in self.encode.children():
+                for param in child:
+                    param.requires_grad = False
 
     def forward(self, x):
+        """
+        The forward propagation for the neural network layers in the __init__()
 
+        || gate(x)                ||
+        || encoder(x)             ||
+        || decoder(x)             ||
+        || conv 1x1 (x)           ||
+        || Sigmoid(x)             ||
+
+        Args:
+            x (torch.Tensor): The minibatch which is to be run through the
+            network. Should be of shape (BATCH_SIZE, 2, HEIGHT, WIDTH), where
+            BATCH_SIZE is the number of samples in each batch, HEIGHT and WIDTH
+            are the pixel dimensions of the input samples.
+
+        Returns:
+            A torch.Tensor with shape of (BATCH_SIZE, 1, HEIGHT, WIDTH), where
+            BATCH_SIZE is the number of samples in each batch, HEIGHT and WIDTH
+            are the pixel dimensions of the input samples.
+        """
+        x = self.gate(x)
+        skip = [x]
+        # encoder
+        for layer in self.encode:
+            x = layer(x)
+            skip.append(x)
+
+        # decoder
+        skip = skip[::-1][1:]  # Reverse skip for easy indexing, dont use first
+        for i, upsample, layer in zip(range(len(skip)+1),
+                                      self.decode_upsample,
+                                      self.decode):
+            x = upsample(x)
+
+            x = torch.cat((x, skip[i]), dim=1)
+            x = layer(x)
+
+        x = self.decode[-1](x)
+        if self.masks:
+            x = torch.cat((x.softmax(dim=0), 1 - x.softmax(dim=0)), dim=1)
         return x
