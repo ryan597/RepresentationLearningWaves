6f791868cf111ba57fc11475553e4263b786d1cf
Ryan <ryan.smith@ucdconnect.ie>
2022-10-21 10:29:27 +0100

    rename

--- segtrain.sh
+++ segtrain.sh
@@ -1,9 +1,10 @@
 #!bin/bash
 
-#SBATCH --time=5:00:00
+#SBATCH --job-name=seg
+#SBATCH --time=2:00:00
 #SBATCH --nodes=1
-#SBATCH --account ndear024a
-#SBATCH --partition GpuQ
+#SBATCH --account=ndear024a
+#SBATCH --partition=GpuQ
 #SBATCH --ntasks-per-node=2
 
 module purge
@@ -19,22 +20,23 @@ export NCCL_SOCKET_IFNAME=eth0
 export NCCL_IB_DISABLE=1
 
 TRAIN_PATH="data"
-VALID_PATH="data/test"
-
+VALID_PATH="data"
+SIZE=512
 BATCH_SIZE=5
 MASKS=True
-STEP=1
-SEQ_LENGTH=3
-FREEZE=0
-SIZE=512
 LR=0.0001
+
+FREEZE=0
+
+STEP=1
+SEQ_LENGTH=5
 LAYERS=50
-CHECKPOINT="outputs/lightning_logs/version/"
+CHECKPOINT="outputs/lightning_logs/version_1013214/checkpoints/epoch=39-step=2360.ckpt"
 
 echo "--train_path $TRAIN_PATH --valid_path $VALID_PATH --test_path $VALID_PATH --batch_size $BATCH_SIZE \
     --masks $MASKS --step $STEP --seq_length $SEQ_LENGTH --freeze $FREEZE --size $size \
     --lr $LR --layers $LAYERS --checkpoint $CHECKPOINT"
 
-python3 train.py --train_path $TRAIN_PATH --valid_path $VALID_PATH --test_path $VALID_PATH --batch_size $BATCH_SIZE \
+srun python3 train.py --train_path $TRAIN_PATH --valid_path $VALID_PATH --test_path $VALID_PATH --batch_size $BATCH_SIZE \
     --masks $MASKS --step $STEP --seq_length $SEQ_LENGTH --freeze $FREEZE --size $size \
     --lr $LR --layers $LAYERS --checkpoint $CHECKPOINT
