7c92d8a2fd0b635ab1f11413beba491d902f43dc
Ryan Smith <15311336@login1.compute>
2023-03-10 14:10:58 +0000

    update slurm for sonic

--- segtrain.sh
+++ segtrain.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 #SBATCH --job-name=RLW
-#SBATCH --time=2:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --nodes=1
-#SBATCH --account=ndear024a
-#SBATCH --partition=GpuQ
+
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
 #SBATCH --ntasks-per-node=2
-#SBATCH --exclude=n[363,364,368]
+#SBATCH  --cpus-per-task=18
 
 ACCELERATOR="gpu"
 DEVICES=2
 NODES=1
 
-TRAIN_PATH="data"
-VALID_PATH="data/test"
+TRAIN_PATH="../scratch"
+VALID_PATH="../scratch/test"
 SIZE=512
 BATCH_SIZE=4
-MASKS=True
+MASKS=False
 LR=0.001
 
 BACKBONE=$1
@@ -33,19 +34,12 @@ echo freeze: $FREEZE
 echo checkpoint: $CHECKPOINT
 
 
-module purge
-module load intel/2019u5
-module load cuda/11.3
-module load cudnn
-module load conda/2
-source activate rlwave
-
-export PYTHONFAULTHANDLER=1
-export NCCL_DEBUG=INFO
-export NCCL_SOCKET_IFNAME=eth0
-export NCCL_IB_DISABLE=1
+#module purge
+#module load anaconda/3.5.2
+source ~/.bashrc
+conda activate rlwave
 
 srun python3 train.py --train_path $TRAIN_PATH --valid_path $VALID_PATH --test_path $VALID_PATH --batch_size $BATCH_SIZE \
-    --masks $MASKS --step $STEP --seq_length $SEQ_LENGTH --freeze $FREEZE --size $SIZE --backbone $BACKBONE \
+    --no-masks --step $STEP --seq_length $SEQ_LENGTH --freeze $FREEZE --size $SIZE --backbone $BACKBONE \
     --lr $LR --layers $LAYERS --accelerator $ACCELERATOR --devices $DEVICES --num_nodes $NODES #--checkpoint $CHECKPOINT
-    #--testing True
+    #--testing
